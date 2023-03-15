a9e68403fcdfeda6645211c8655dcb78b64adb33
yinan <st171243@stud.uni-stuttgart.de>
2023-03-15 13:59:17 +0100

    update 3.15

--- temporal_pattern/run.py
+++ temporal_pattern/run.py
@@ -9,7 +9,7 @@ import evaluation
 import temporal_pattern_lookout
 parser = argparse.ArgumentParser()
 parser.add_argument('--dataset', default='icews14', choices=['icews14', 'wikidata_TA', 'icews15'], help='Knowledge graph dataset')
-<<<<<<< HEAD
+
 # parser.add_argument('--pattern', default='evolve'
 #                     , choices=['symmetric', 'temporal symmetric', 'inverse'
 #         , 'temporal inverse', 'implication', 'temporal implication', 'evolve'])
@@ -21,7 +21,7 @@ temporal_pattern_lookout.main(args.dataset)
 for p in ['symmetric', 'temporal symmetric', 'inverse'
         , 'temporal inverse', 'implication', 'temporal implication', 'evolve']:
     evaluation.main(args.dataset, p, args.threshold)
-=======
+
 parser.add_argument('--pattern', default='evolve'
                     , choices=['symmetric', 'temporal symmetric', 'inverse'
         , 'temporal inverse', 'implication', 'temporal implication', 'evolve'])
@@ -29,5 +29,5 @@ args = parser.parse_args()
 
 temporal_pattern_lookout.main(args.dataset)
 evaluation.main(args.dataset, args.pattern)
->>>>>>> 0727151 (update 3.15)
+
 
